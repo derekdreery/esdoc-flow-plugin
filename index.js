31cbbaa42af990c0b3c409861eaacfedf6e21833
derekdreery <rodododouk@hotmail.com>
2016-11-02 12:47:00 +0000

    Update index.js
    Use .babelrc if it is present

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
 const babel = require('babel-core');
 
-const babelOpts = {
+let babelOpts = {
   plugins: ['transform-flow-strip-types', 'syntax-jsx']
 };
+
+try {
+  fs.accessSync('./.babelrc', fs.F_OK);
+  babelOpts = JSON.parse(fs.readFileSync('./.babelrc'));
+} catch (e) {}
 
 exports.onHandleCode = (event) => {
   try {
